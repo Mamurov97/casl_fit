0a0a91b1a2fbd55fdc8a31ee4cd83a9487d7e28f
turgu <turgunboyev7212@gmail.com>
2025-04-05 16:09:20 +0500

    bug fix

--- lib/presentation/routes/coordinator.dart
+++ lib/presentation/routes/coordinator.dart
@@ -1,5 +1,4 @@
 import 'package:casl_fit/application/home/profile/weight_height/weight_height_bloc.dart';
-import 'package:casl_fit/infrastructure/dto/models/tariff/tariff_model.dart';
 import 'package:casl_fit/presentation/components/screens/coming_soon_page.dart';
 import 'package:casl_fit/presentation/pages/profile/weight_height/weight_height_page.dart';
 import 'package:flutter/material.dart';
